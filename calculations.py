a76b79e0526960b7d3acc5ad6d1d55c28c22a2c9
Dan <danscriptedyt@gmail.com>
2026-03-11 15:55:02 +0000

    display_operative func now shows stats and weapon stats

--- calculations.py
+++ calculations.py
@@ -18,12 +18,25 @@ operatives = data[team_name]["operatives"]
 # print(data["angels of death"]["operatives"]["Intercessor Warrior"]["movement"])
 
 def display_operative(operative):
-     print(f"Name: {operative["name"]}")
-     print(f"Movement: {operative["movement"]}")
-     print(f"Actions APL: {operative["apl"]}")
-     print(f"Defence: {operative["defence"]}")
-     print(f"Save: {operative["save"]}")
-     print(f"Wounds: {operative["wounds"]}")
+     print(f"{operative['name']}")
+     print(f"{'MOV' :<6} {'APL' :<6} {'DEF' :<6} {'SAVE' :<6} {'WOUNDS' :<6}")
+     print(f"{operative['movement']:<6} {operative['apl']:<6} {operative['defence']:<6} {operative['save']:<6} {operative['wounds']:<6}")
+     print(f"WEAPONS")
+     for weapon_name, weapon in operative["weapons"].items():
+        print(weapon_name)
+        print(f"{'  ATK:':>4}{weapon['attacks']} {'BS:':<4}{weapon['bs']} {'DAM:':<4}{weapon['normal_damage']}")
+        print(f"  Rules: {', '.join(weapon['special_rules'])}")
+        
+     
+    
+     
+
+# print(f"Name: {operative['name']} \nMOV: \n{operative['movement']}")
+#      print(f"================================================")
+#      print(f"Actions APL: {operative['apl']}")
+#      print(f"Defence: {operative['defence']}")
+#      print(f"Save: {operative['save']}")
+#      print(f"Wounds: {operative['wounds']}")
 
 
-display_operative(data["angels of death"]["operatives"]["Intercessor Warrior"])
+display_operative(operatives["Assault Intercessor Sergeant"])
