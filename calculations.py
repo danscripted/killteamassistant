b9856c49e51ed46b7f0172a4c9019ffafb758c7d
Dan <danscriptedyt@gmail.com>
2026-03-11 11:46:56 +0000

    Added display operative stats function

--- calculations.py
+++ calculations.py
@@ -1,7 +1,6 @@
 import json
 import random
 
-import json
 data = json.load(open("operatives.json", encoding="utf-8"))
 
 
@@ -12,6 +11,19 @@ display_name = team_name.title()
 # Get the operatives sub-dictionary
 operatives = data[team_name]["operatives"]
 
-print(f"Team: {display_name}")
-print(f"Number of operatives: {len(operatives)}")
-print("Operative names:", list(operatives.keys()))
+# print(f"Team: {display_name}")
+# print(f"Number of operatives: {len(operatives)}")
+# print("Operative names:", list(operatives.keys()))
+
+# print(data["angels of death"]["operatives"]["Intercessor Warrior"]["movement"])
+
+def display_operative(operative):
+     print(f"Name: {operative["name"]}")
+     print(f"Movement: {operative["movement"]}")
+     print(f"Actions APL: {operative["apl"]}")
+     print(f"Defence: {operative["defence"]}")
+     print(f"Save: {operative["save"]}")
+     print(f"Wounds: {operative["wounds"]}")
+
+
+display_operative(data["angels of death"]["operatives"]["Intercessor Warrior"])
