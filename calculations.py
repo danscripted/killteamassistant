d154f50457ef641f77a86685a0261439f23308e2
Dan <danscriptedyt@gmail.com>
2026-03-12 11:29:11 +0000

    cleaned older comments

--- calculations.py
+++ calculations.py
@@ -2,21 +2,9 @@ import json
 import random
 
 data = json.load(open("operatives.json", encoding="utf-8"))
-
-
 team_name = list(data.keys())[0]
-
-display_name = team_name.title()
-
 # Get the operatives sub-dictionary
 operatives = data[team_name]["operatives"]
-
-
-# print(f"Team: {display_name}")
-# print(f"Number of operatives: {len(operatives)}")
-# print("Operative names:", list(operatives.keys()))
-
-# print(data["angels of death"]["operatives"]["Intercessor Warrior"]["movement"])
 
 def display_operative(operative):
      print(f"{operative['name']}")
