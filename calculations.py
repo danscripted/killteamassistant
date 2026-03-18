c87516392e413c060ca6d0af505b0be4fe46961a
Dan <danscriptedyt@gmail.com>
2026-03-18 16:07:32 +0000

    Reorg code so it calls the functions in correct order

--- calculations.py
+++ calculations.py
@@ -7,26 +7,6 @@ team_name = list(data.keys())[0]
 # Get the operatives sub-dictionary
 operatives = data[team_name]["operatives"]
 
-def operative_menu():
-    print(f"Angels of Death - Kill Team Assistant")
-    print(f"======================================")
-    
-    for index, name in enumerate(operatives, start=1):
-        print(f"{index}. {name}")
-    selection = input("Select an operative:")
-    operatives_list = list(operatives.keys())
-    chosen = operatives_list[int(selection) - 1]
-
-    print(f"You have selected {chosen}")
-    
-
-    for index, name in enumerate(operatives[chosen]["weapons"], start=1):
-        print(f"{index}. {operatives[chosen]["weapons"]}")
-    weapons_list = list
-    selection_weapon = input("Select a weapon:")
-
-operative_menu()
-
 def display_operative(operative):
      print(f"{operative['name']}")
      print(f"{'MOV' :<6} {'APL' :<6} {'DEF' :<6} {'SAVE' :<6} {'WOUNDS' :<6}")
@@ -36,27 +16,6 @@ def display_operative(operative):
         print(weapon_name)
         print(f"{'  ATK:':>4}{weapon['attacks']} {'BS:':<4}{weapon['bs']} {'DAM:':<4}{weapon['normal_damage']}")
         print(f"  Rules: {', '.join(weapon['special_rules'])}")
-        
-display_operative(operatives["Assault Intercessor Sergeant"])     
-    
-# def simulate_attack(weapon):
-#     print(f"Rolling attack:")
-#     print("=========================")
-#     normal_hits = 0
-#     crit_hits = 0
-#     for i in range(weapon['attacks']):
-#        roll = random.randint(1, 6)
-#        if roll == 6:
-#         crit_hits += 1
-#        elif roll >= weapon['bs']:
-#             normal_hits += 1
-#     print(f"Normal hits: {normal_hits} Crit hits: {crit_hits}")
-#     total_damage = (normal_hits * weapon['normal_damage'] + (crit_hits * weapon['crit_damage']))
-#     print (f"{total_damage}")    
-
-# simulate_attack(operatives["Assault Intercessor Sergeant"]["weapons"]["Hand Flamer"])
-
-
 
 def simulate_attack(weapon):
     print(f"Rolling attack:")
@@ -79,6 +38,54 @@ def simulate_attack(weapon):
         total_damage_simmed += total_damage   
     
     average_damage = total_damage_simmed / simulations
-    print(f"Average damage over {simulations} simulations: {average_damage:.2f}")    
+    print(f"Average damage over {simulations} simulations: {average_damage:.2f}") 
 
-simulate_attack(operatives["Assault Intercessor Sergeant"]["weapons"]["Hand Flamer"])
+def operative_menu():
+    print(f"Angels of Death - Kill Team Assistant")
+    print(f"======================================")
+    
+    for index, name in enumerate(operatives, start=1):
+        print(f"{index}. {name}")
+    selection = input("Select an operative:")
+    operatives_list = list(operatives.keys())
+    chosen = operatives_list[int(selection) - 1]
+
+    print(f"You have selected {chosen}")
+    
+
+    # FIXED
+    weapons = operatives[chosen]["weapons"]
+    for index, name in enumerate(weapons, start=1):
+        print(f"{index}. {name}")
+    weapons_list = list(weapons.keys())
+    selection_weapon = input("Select a weapon: ")
+    chosen_weapon = weapons_list[int(selection_weapon) - 1]
+    display_operative(operatives[chosen]) 
+    simulate_attack(operatives["Assault Intercessor Sergeant"]["weapons"]["Hand Flamer"])
+
+operative_menu()
+
+
+        
+#    
+    
+# def simulate_attack(weapon):
+#     print(f"Rolling attack:")
+#     print("=========================")
+#     normal_hits = 0
+#     crit_hits = 0
+#     for i in range(weapon['attacks']):
+#        roll = random.randint(1, 6)
+#        if roll == 6:
+#         crit_hits += 1
+#        elif roll >= weapon['bs']:
+#             normal_hits += 1
+#     print(f"Normal hits: {normal_hits} Crit hits: {crit_hits}")
+#     total_damage = (normal_hits * weapon['normal_damage'] + (crit_hits * weapon['crit_damage']))
+#     print (f"{total_damage}")    
+
+# simulate_attack(operatives["Assault Intercessor Sergeant"]["weapons"]["Hand Flamer"])
+
+
+
+   
